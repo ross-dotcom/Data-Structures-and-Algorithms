ec41e3f8f1e35034c18d210eada7b5a7599f1265
ross-dotcom <wross843@gmail.com>
2020-06-03 22:39:47 +0200

    How To: Bubble Sort Algorithm.

---
+++ bubble_sort.py
@@ -0,0 +1,21 @@
+"""
+    Bubble sort algorithm.
+"""
+
+def bubble_sort(arr):
+    
+    length = len(arr)-1
+    while length >= 1:
+        position = 0
+        for i in range(length):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                position = i
+        length = position
+        
+    return arr
+
+if __name__ == '__main__':
+
+    my_array = [10, 4, 6, 8, 5, 2, 4, 6, 8, 9, 22]
+    print(bubble_sort(my_array))
